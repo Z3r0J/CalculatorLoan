adff347110a29444f49721cc4431478d9e4af7d3
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-05-22 17:23:30 -0400

    Clean the Project and Update Bootstrap

--- CalculatorLoan/Startup.cs
+++ CalculatorLoan/Startup.cs
@@ -50,7 +50,7 @@ namespace CalculatorLoan
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=LoanCalculator}/{action=Index}/{id?}");
             });
         }
     }
